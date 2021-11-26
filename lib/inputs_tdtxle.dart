151eedda0cffe44e2903469bb37c9a69724fc53a
LuisDeLaValier <TDTxLE@gmail.com>
2021-11-25 18:45:30 -0600

    Imagen Perfil

--- lib/inputs_tdtxle.dart
+++ lib/inputs_tdtxle.dart
@@ -1,6 +1,6 @@
 library inputs_tdtxle;
 
 
-// export 'package:inputs_tdtxle/inputs/img_perfil.dart';
+export 'package:inputs_tdtxle/inputs/img_perfil.dart';
 // export 'package:inputs_tdtxle/inputs/chip_imput.dart';
 
