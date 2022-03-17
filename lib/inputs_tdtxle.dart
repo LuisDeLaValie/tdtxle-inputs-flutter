a25a74a9edd9bc32ba01b26033b6b89794105819
Luis Emilio <tdtxle@gmail.com>
2022-03-16 18:08:57 -0600

    .

--- lib/inputs_tdtxle.dart
+++ lib/inputs_tdtxle.dart
@@ -3,4 +3,5 @@ library tdtxle_inputs_flutter;
 
 export 'package:tdtxle_inputs_flutter/inputs/img_perfil.dart';
 export 'package:tdtxle_inputs_flutter/inputs/chip_imput.dart';
+export 'package:tdtxle_inputs_flutter/inputs/select_imput.dart';
 
